301c7aa8f76888739552b51b12cd49a4e5d2b086
mansyar <m.ansyarafi@gmail.com>
2026-09-07 07:49:59 +1000

    feat(bootstrap): add prisma 7 with better auth schema and initial migration

---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { betterAuth } from "better-auth/minimal";
+import { nextCookies } from "better-auth/next-js";
+import { prisma } from "./db";
+
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	plugins: [nextCookies()],
+});
